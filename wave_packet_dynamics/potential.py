9b99262e00504e4c99a2f4d5e28059101ce4245d
Rastow <robert.grzonka@fau.de>
2021-08-24 21:34:23 +0200

    Add predefined potentials Test reST/TeX math in docstring

--- wave_packet_dynamics/potential.py
+++ wave_packet_dynamics/potential.py
@@ -1,1 +1,49 @@
-"""Module with predefined potential functions."""
+"""Submodule with predefined potential functions."""
+import numpy as np
+
+
+def harmonic(x: np.ndarray, k: float, x0: float) -> np.ndarray:
+    """
+    Represents a harmonic (parabola) potential.
+
+    .. math::
+
+        V\left( x \right) =0.5\cdot k\cdot \left( x-x_{ 0 } \right)
+
+    :param numpy.ndarray x: coordinate array for computation of function values
+    :param float k: force constant
+    :param float x0: coordinate of the potential valley (zero point)
+
+    :return: array with function values of the harmonic potential
+    :rtype: numpy.ndarray
+    """
+    return 0.5 * k * (x - x0) ** 2
+
+
+def wall(x: np.ndarray, height: float, width: float, x0: float) -> np.ndarray:
+    """
+    Represents a (rectangular) hard wall potential step.
+
+    .. math::
+
+        V\left( x \right) =
+        \left\{\begin{array}{lcr}
+        h, & \text{for} & x_{0}-\frac{ w }{ 2 }<x<x_{0}+\frac{ w }{ 2 } \\
+        0 & \text{for} &  \\
+        \end{array}\right\}
+
+    :param numpy.ndarray x: coordinate array for computation of function values
+    :param float height: height of the potential step
+    :param float width: width of the potential step
+    :param float x0: coordinate of the potential center
+
+    :return: array with function values of the wall potential
+    :rtype: numpy.ndarray
+    """
+    y = np.empty_like(x)
+    for index, coord in enumerate(x):
+        if x0 - width / 2 < coord < x0 + width / 2:
+            y[index] = height
+        else:
+            y[index] = 0
+    return y
--- wave_packet_dynamics/wave_function.py
+++ wave_packet_dynamics/wave_function.py
@@ -1,4 +1,4 @@
-"""Module with predefined wave functions."""
+"""Submodule with predefined wave functions."""
 import numpy as np
 
 
@@ -6,12 +6,18 @@ def gaussian(x: np.ndarray, sigma: float, x0: float, k0: float) -> np.ndarray:
     """
     Represents a wave packet with gaussian distribution and initial momentum.
 
-    :param np.ndarray x: initial coordinate array which should be used for computing the function values
+    .. math::
+
+        f\left( x \right)=\left( 2\pi\sigma^{ 2 } \right)^{ -1/4 }
+        \cdot e^{ -\left( x-x_{ 0 } \right) ^{ 2 }/4
+        \cdot \sigma^{ 2 }}\cdot e^{i\cdot k_{ 0 } x}
+
+    :param numpy.ndarray x: coordinate array for computation of function values
     :param float sigma: full width at half maximum of the gaussian distribution
     :param float x0: initial most probable coordinate
     :param float k0: initial wave number of the matter wave
 
     :return: array with function values of the gaussian wave packet
-    :rtype: np.ndarray
+    :rtype: numpy.ndarray
     """
     return (2 * np.pi * sigma ** 2) ** -0.25 * np.exp(-(x - x0) ** 2 / (4 * sigma ** 2)) * np.exp(1j * k0 * x)
