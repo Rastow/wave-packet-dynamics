b7b768ff1b85c92bd76b511cfcaecbb2af9fc02a
Rastow <robert.grzonka@fau.de>
2021-09-06 10:03:13 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='wave_packet_dynamics',
-    version='0.0.4',
+    version='0.1.0',
     url='https://github.com/Rastow/wave-packet-dynamics',
     author='Robert Grzonka',
     author_email='robert.grzonka@fau.de',
