31c03dcfbeb6385fdb37c9c0bb8dae54875ff083
Rastow <robert.grzonka@fau.de>
2021-09-06 10:14:18 +0200

    Add source suffix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,11 @@ extensions = [
     "myst_nb",
 ]
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.ipynb': 'myst-nb',
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
